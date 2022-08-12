240cb799d71b73b183e39e10ebc59e67aba7882b
tuananh168 <tuananh16895@gmail.com>
2022-08-12 21:45:46 +0700

    Push code

--- 32E_Nhom19_CapstoneProject/app/controller/index.js
+++ 32E_Nhom19_CapstoneProject/app/controller/index.js
@@ -12,19 +12,19 @@ function layDanhSachSanPhamAPI() {
 
 
     // Xử lý thành công
-    promise.then(function(result) {
+    promise.then(function (result) {
         console.log(result.data.content);
         renderTable(result.data.content);
     });
 
-    promise.catch(function(error) {
+    promise.catch(function (error) {
         console.log(error);
     });
 }
 
 // Gọi hàm dữ liệu khi trang web vừa load xong
 
-window.onload = function() {
+window.onload = function () {
     layDanhSachSanPhamAPI();
 
 };
@@ -46,7 +46,7 @@ function renderTable(arrProduct) {
                         <p>${item.shortDescription}
                         </p>
                         <div class="row">
-                            <a class="col-6" href="../views/detail.html?productid=${item.id}">Buy now</a>
+                            <a class="col-6" href="./app/views/detail.html?productid=${item.id}">Buy now</a>
                             <span class="col-6">${item.price}$</span>
                         </div>
                     </div>
